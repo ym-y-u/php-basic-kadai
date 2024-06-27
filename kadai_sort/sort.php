3ac4e4673c84c9af5fa0cde1dba6983274392f6f
ym-y-u <166213577+ym-y-u@users.noreply.github.com>
2024-06-27 19:52:18 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -8,30 +8,26 @@
 
 <body>
     <p>
-        <?php
+     <?php
 
-        function sort_2way($array,$oder) {
-        if ($oder == "asc") {
-            sort($array);
-        } else if ($oder == "desc"){
-            rsort($array);
-        }
-        return $array;
+     function sort_2way($array,$oder) {
+     if ($oder == "asc") {
+       sort($array);
+     } else if ($oder == "desc"){
+       rsort($array);
+     }
+       foreach ($array as $num) {
+        echo $num . "<br>";
+       }
     }
 
     $nums =[15,4,18,23,10];
 
     echo "昇順にソートします。<br>";
-    $sorted_nums_asc = sort_2way($nums,"asc");
-    foreach ($sorted_nums_asc as $num) {
-        echo $num . "<br>";
-    }
+    sort_2way($nums,"asc");
 
     echo "降順にソートします。<br>";
-    $sorted_nums_asc = sort_2way($nums,"desc");
-    foreach ($sorted_nums_asc as $num) {
-        echo $num . "<br>";
-    }
+    sort_2way($nums,"desc");
 
         ?>
     </p>
