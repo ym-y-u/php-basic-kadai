8b318c513b99ddfcee6b6cb1ed194ac23951da14
ym-y-u <166213577+ym-y-u@users.noreply.github.com>
2024-07-06 23:00:01 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,28 @@
+<?php
+// セッションを開始
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang = "ja">
+
+<head>
+  <meta charset = "UTF-8">
+  <title> PHP基礎編 </title>
+</head>
+
+<body>
+  <h2> 登録が完了しました。 </h2>
+  <p> 社員情報がデータベースに保存されました。 </p>
+  <button id = "back-btn" onclick = "location.href = 'form.php';"> 戻る </button>
+
+<?php
+// セッション変数を初期化
+$_SESSION = array();
+
+// セッションを終了
+session_destroy();
+?>
+</body>
+
+</html>
